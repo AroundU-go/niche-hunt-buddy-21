5004dee57cb7c47a1297218bbfbee2ef082a1850
uddipangoswami <uddipangoswami101@gmail.com>
2026-07-05 16:43:38 +0530

    fix: ignore TanStack Router redirect errors in errorMiddleware to prevent 500 error pages on sign-out

--- src/start.ts
+++ src/start.ts
@@ -1,5 +1,6 @@
 import { createStart, createMiddleware } from "@tanstack/react-start";
 import { clerkMiddleware } from "@clerk/tanstack-react-start/server";
+import { isRedirect } from "@tanstack/react-router";
 
 import { renderErrorPage } from "./lib/error-page";
 
@@ -7,6 +8,15 @@ const errorMiddleware = createMiddleware().server(async ({ next }) => {
   try {
     return await next();
   } catch (error) {
+    if (
+      isRedirect(error) ||
+      (error != null &&
+        typeof error === "object" &&
+        (("isRedirect" in error && error.isRedirect) ||
+          ("type" in error && error.type === "redirect")))
+    ) {
+      throw error;
+    }
     if (error != null && typeof error === "object" && "statusCode" in error) {
       throw error;
     }
